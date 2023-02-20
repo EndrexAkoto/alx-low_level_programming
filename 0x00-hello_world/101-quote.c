964a3d88fa5865f1e7f99171ca8fa4e5060e4368
EndrexAkoto <martinakoto3@gmail.com>
2023-02-20 05:09:36 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,11 +6,11 @@
  *
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
- * Return: Always 0 (Success)
+ * Return 1(Success)
  */
 
 int main(void)
 {
-        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        fput("1 and that piece of art is useful\" - "Dora Korpar, 2015-10-19\n", 59);
         return (1);
 }
