2e6016e55f684f8ad2c6065cebecbb2acc729a2f
EndrexAkoto <martinakoto3@gmail.com>
2023-02-21 15:02:59 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+  * main -prints _putcher a new line
+  * Return: Always 0
+  */
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putcher('_');
+	_putcher('p');
+	_putcher('u');
+	_putcher('t');
+	_putcher('c');
+	_putcher('h');
+	_putcher('a');
+	_putcher('r');
+	_putcher('\n');
+
 	return (0);
-}
+
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
-* print_alphabet - print all alphabet in lowercase
-*/
-void print_alphabet(void)
+  * print_alphabet- prints the alpha in lowercase
+  */
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar('\n');
+	char la;
+
+	for (la= 'a'; la <= 'z'; la++)
+		_putcher(la);
+
+	_putcher('\n');
 }
