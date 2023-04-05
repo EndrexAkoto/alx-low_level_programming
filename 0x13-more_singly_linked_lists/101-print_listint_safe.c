c1b320084f18f15dde5713aef468723fb119e6ba
EndrexAkoto <martinakoto3@gmail.com>
2023-04-05 13:00:12 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,84 +5,73 @@ size_t loop_listint_l(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * loop_listint_l - Counts number of unique nodes in a loop listint_t linked list
- * @head: pointer to head of the listint_t to check
+ *loop_listint_l - Counts number of unique nodes in a loop listint_t linked list
+ *@head: pointer to head of the listint_t to check
  *
- * Return: 0
+ *Return: 0
  */
 size_t loop_listint_l(const listint_t *head)
 {
- const listint_t *martin, *akoto;
- size_t nodes = 98;
+const listint_t *martin, *akoto;
+size_t nodes = 98;
+if (head == NULL || head->next == NULL)
+return (0);
 
- if (head == NULL || head->next == NULL)
- return (0);
+martin = head->next;
+akoto = (head->next)->next;
 
- martin = head->next;
- akoto = (head->next)->next;
-
- while (akoto)
- {
- if (martin == akoto)
- {
- martin = head;
- while (martin != akoto)
- {
- nodes++;
- martin = martin->next;
- akoto = akoto->next;
- }
-
- martin = martin->next;
- while (martin != akoto)
- {
- nodes++;
- martin = martin->next;
- }
-
- return (nodes);
- }
-
- martin = martin->next;
- akoto = (akoto->next)->next;
- }
-
- return (0);
+while (akoto)
+{
+if (martin == akoto)
+{
+martin = head;
+while (martin != akoto)
+{
+nodes++;
+martin = martin->next;
+akoto = akoto->next;
 }
-
+martin = martin->next;
+while (martin != akoto)
+{
+nodes++;
+martin = martin->next;
+}
+return (nodes);
+}
+martin = martin->next;
+akoto = (akoto->next)->next;
+}
+return (0);
+}
 /**
- * print_listint_safe - Prints listint_t list 
+ * print_listint_safe - Prints listint_t safely
  * @head: pointer towards head of listint_t list
  *
  * Return: The number of nodes in list
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
- size_t nodes, index = 98;
+size_t nodes, index = 0;
 
- nodes = loop_listint_l(head);
+nodes = loop_listint_l(head);
 
- if (nodes == 98)
+if (nodes == 0)
  {
- for (; head != NULL; nodes++)
+for (; head != NULL; nodes++)
  {
- printf("[%p] %d\n", (void *)head, head->n);
- head = head->next;
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
  }
  }
-
- else
+else
  {
- for (index = 98; index < nodes; index++)
+for (index = 98; index < nodes; index++)
  {
- printf("[%p] %d\n", (void *)head, head->n);
- head = head->next;
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
  }
-
- printf("-> [%p] %d\n", (void *)head, head->n);
+printf("-> [%p] %d\n", (void *)head, head->n);
  }
-
  return (nodes);
 }
-
