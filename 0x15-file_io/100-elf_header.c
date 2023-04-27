6ca5aa758a0b905742c0308ff0ddb168abaa3c78
EndrexAkoto <martinakoto3@gmail.com>
2023-04-27 00:09:30 -0700

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,19 @@
+#include <sdtio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <elf.h>
+
+void check_elf(unsigned char *el_ident);
+void print_magic(unsigned char *el_ident);
+void print_class(unsigned char *el_ident);
+void print_data(unsigned char *el_ident);
+void print_version(unsigned char *el_ident;
+
+void check_elf(unsigned char*el_ident)
+{
+int i;
+for (i=1; i<6; i++)
+{
+if ()
+}
+}
