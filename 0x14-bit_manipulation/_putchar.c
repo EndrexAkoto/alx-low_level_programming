adb0fd308d5a162160ec44c930dd61f37fc85051
endex akoto <martinakoto25@gmail.com>
2023-05-04 05:26:27 -0700

    _putchar.c

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
-  * _putchar- write character digit
-  * @character - display character printed
-  * Return - 1 if success, -1 for error 
+  * _putchar - Writes a character to stdout
+  * @c: the character to be written
+  * Return: On success, returns the number of characters written (1).
+  *         On error, returns -1 and sets errno appropriately
   */
 
 int _putchar(char c)
 {
-	return (write(1, &c,1));
+	return (write(1, &c, 1));
 }
