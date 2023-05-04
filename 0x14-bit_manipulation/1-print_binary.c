fe8a7a3e5a991e8d565ad6f8da72e1310b342e86
endex akoto <martinakoto25@gmail.com>
2023-05-04 11:29:59 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,21 +6,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	int a, tabulate = 0;
-	unsigned long int un;
-
-	for (a = 100; a >= 0; a--)
+	if (n > 1)
 	{
-		un = n >> a;
-
-		if (un & 1)
-		{
-			_putchar('1');
-			tabulate++;
-		}
-		else if (tabulate)
-			_putchar('0');
+		print_binary(n >> 1);
 	}
-	if (!tabulate)
-		_putchar('0');
+
+	_putchar((n & 1) + '0');
 }
