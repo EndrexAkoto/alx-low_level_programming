4fec0cd3de86b6e7f9cb8c97801a8d6e84107f80
EndrexAkoto <martinakoto3@gmail.com>
2023-10-16 09:37:16 -0700

    change

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,35 +14,36 @@
 int binary_search(int *array, size_t size, int value)
 {
 	size_t i, left, right;
-	
+
 	/* Check if array is NULL */
 	if (array == NULL)
 		return (-1);
 
 	/* Performs binary search */
 	for (left = 0, right = size - 1; right >= left;)
-	{
+{
 		printf("Searching in array: ");
 		for (i = left; i < right; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
 		
-		/* Calculate middle index */
+	/* Calculate middle index */
 		i = left + (right - left) / 2;
 		
-		/* If middle element is the value */
+	/* If middle element is the value */
 		if (array[i] == value)
-			/* Return the index */
+
+	/* Return the index */
 			return (i);
 		
-		/* If middle element is greater than the value */
+	/* If middle element is greater than the value */
 		if (array[i] > value)
-			/* Update right boundary */
+	/* Update right boundary */
 			right = i - 1;
 		else
-			/* Otherwise, update left boundary */
+	/* Otherwise, update left boundary */
 			left = i + 1;
-	}
+}
 	/* Value not found, return -1 */
 	return (-1);
 }
